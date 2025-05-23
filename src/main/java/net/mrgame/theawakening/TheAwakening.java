b23b84706baa29143557f3dfbd3031de7f80f350
MrGame20007 <aidencolla9@gmail.com>
2025-05-23 15:33:14 +0200

    Initial Commit

---
+++ src/main/java/net/mrgame/theawakening/TheAwakening.java
@@ -0,0 +1,16 @@
+package net.mrgame.theawakening;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TheAwakening implements ModInitializer {
+	public static final String MOD_ID = "theawakening";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/mrgame/theawakening/TheAwakeningClient.java
@@ -0,0 +1,10 @@
+package net.mrgame.theawakening;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TheAwakeningClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
